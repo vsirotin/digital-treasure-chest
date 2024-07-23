8c88147cde412b3d15a19168bbbf50cabe626cac
viktorsirotin <vxod@sirotin.eu>
2024-07-23 15:05:31 +0200

    chore: Browser local storage pilot variant 1. WIP!

--- projects/browser-local-storage/src/public-api.ts
+++ projects/browser-local-storage/src/public-api.ts
@@ -2,5 +2,4 @@
  * Public API Surface of browser-local-storage
  */
 
-export * from './lib/browser-local-storage.service';
-export * from './lib/browser-local-storage.component';
+export * from './lib/db-agent';
