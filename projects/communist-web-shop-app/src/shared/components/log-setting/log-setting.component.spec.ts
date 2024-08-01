cc7bb561d8292dbfd07fe1fc6eb840ca428dda0d
viktorsirotin <vxod@sirotin.eu>
2024-08-01 19:22:47 +0200

    chore: Logging testing TMP state. WIP!

--- projects/communist-web-shop-app/src/shared/components/log-setting/log-setting.component.spec.ts
+++ projects/communist-web-shop-app/src/shared/components/log-setting/log-setting.component.spec.ts
@@ -2,10 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogSettingComponent } from './log-setting.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
+import { LoggerFactory } from '@vsirotin/log4ts';
+import { DebugElement } from '@angular/core';
+import { MatRadioButton, MatRadioChange } from '@angular/material/radio';
 
 describe('LogSettingComponent', () => {
   let component: LogSettingComponent;
   let fixture: ComponentFixture<LogSettingComponent>;
+  let buttonUpdate: DebugElement;
+  let buttonReset: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +20,10 @@ describe('LogSettingComponent', () => {
     
     fixture = TestBed.createComponent(LogSettingComponent);
     component = fixture.componentInstance;
+
+    buttonUpdate = fixture.debugElement.query(By.css('#bt-update'));
+    buttonReset= fixture.debugElement.query(By.css('#bt-reset'));
+
     fixture.detectChanges();
   });
 
@@ -22,16 +31,101 @@ describe('LogSettingComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should handle button click with id "bt-update"', async () => {
-    spyOn(component, 'onUpdate'); 
-    const button = fixture.debugElement.query(By.css('#bt-update'));
-    console.log(button);
-    button.nativeElement.click();
-    await fixture.whenStable();
-    //expect(component.onUpdate).toHaveBeenCalled();
-    expect(button.nativeElement.disabled).toBe(true)
-
-    const button1 = fixture.debugElement.query(By.css('#bt-update')).nativeElement;
-    console.log(button1);
+  afterEach(() => {
+    LoggerFactory.clearAllLoggers();
   });
+
+  // function selectButtonAllLevels(fixture: ComponentFixture<LogSettingComponent>) {
+  //   console.log("fixture=", fixture);
+  //   const radioButton = fixture.debugElement.queryAll(By.css('mat-radio-button'))[4];
+  //   console.log("radioButton=", radioButton.name);
+  //   radioButton.nativeElement.click();
+  //   fixture.detectChanges();
+  // }
+
+
+  describe('After start (by Default)...', () => {
+    it('should the button Update be disabled"', () => {
+      expect(buttonUpdate.nativeElement.disabled).toBe(true)
+    });
+
+    it('should the button Reset be disabled"', () => {
+      expect(buttonReset.nativeElement.disabled).toBe(true)
+    });
+
+    it('should level error-warn selected"', () => {
+      expect(component.selectedLogLevel).toBe('err-warn');
+    });
+
+    xit('by selection should the onDebugLevelChange called', async () => {
+      spyOn(component, 'onDebugLevelChange'); 
+
+      const radioButtonDebugElement = fixture.debugElement.queryAll(By.css('mat-radio-button'))[4];
+      const radioButton = radioButtonDebugElement.componentInstance as MatRadioButton;
+
+      radioButtonDebugElement.triggerEventHandler('check', {});
+     
+
+      // Ensure the radio button is rendered
+      expect(radioButton).toBeDefined();
+
+
+
+     // Simulate the click event using dispatchEvent
+     radioButton.checked = true;
+     //radioButtonDebugElement.nativeElement.dispatchEvent(new Event('click'));
+     //radioButton._inputElement.nativeElement.dispatchEvent(new Event('click'));
+     radioButton._onInputClick(new Event('click'));
+     const changeDetalis: MatRadioChange = {source: radioButton, value: 'aaa'};
+     //radioButton.change.emit(changeDetalis);
+
+     await radioButtonDebugElement.nativeElement.click();
+      
+      //radioButton.checked = true;
+      //radioButton._inputElement.nativeElement.dispatchEvent(new Event('change'));
+
+
+      fixture.detectChanges();
+      
+      await fixture.whenStable();
+      expect(radioButton.checked).toBeTruthy();
+       // Verify the method call
+      expect(component.onDebugLevelChange).toHaveBeenCalledWith("QQQ")
+
+      
+
+      // Wait for all asynchronous tasks to complete
+      //await fixture.whenStable();
+
+      // Trigger change detection
+      //fixture.detectChanges();
+      //expect(component.onDebugLevelChange).toHaveBeenCalledWith("aaa");
+    });
+
+    xdescribe('after selection the level Loggin set off... ', () => {
+
+      
+
+      it('should the button Update be enabled"', () => {
+        fixture.debugElement.queryAll(By.css('mat-radio-button'))[4].nativeElement.click();
+        fixture.detectChanges();
+        expect(buttonUpdate.nativeElement.disabled).toBe(false)
+      });
+  
+      it('should the button Reset be disabled"', () => {
+        expect(buttonReset.nativeElement.disabled).toBe(true)
+      });
+  
+      it('should level error-debug selected"', () => {
+        expect(component.selectedLogLevel).toBe('err-debug');
+      });
+
+    });
+   
+    
+  });
+
+
 });
+
+
---
+++ projects/communist-web-shop-app/src/shared/components/test/radio-harness-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
+
+/**
+ * @title Testing with MatRadioHarness
+ */
+@Component({
+  selector: 'radio-harness-example',
+  templateUrl: 'radio-harness-example.html',
+  standalone: true,
+  imports: [
+    MatRadioModule
+
+  ],
+})
+export class RadioHarnessExample {}
