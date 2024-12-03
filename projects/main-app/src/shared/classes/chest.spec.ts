5d3f6928a7757e13311a8f0cdd07bbde589f730c
viktorsirotin <vxod@sirotin.eu>
2024-12-03 13:50:36 +0100

    Class Chest implemented and tested- WIP!

--- projects/main-app/src/shared/classes/chest.spec.ts
+++ projects/main-app/src/shared/classes/chest.spec.ts
@@ -1,7 +1,53 @@
 import { Chest } from './chest';
 
 describe('Chest', () => {
-  it('should create an instance', () => {
-    expect(new Chest()).toBeTruthy();
+  let chest: Chest;
+
+  beforeEach(() => {
+    chest = Chest.instance;
+    chest.items.clear(); // Clear items before each test
+  });
+
+  it('should create an instance of Chest', () => {
+    expect(chest).toBeTruthy();
+  });
+
+  it('should return items as an array', () => {
+    chest.addItems([1, 2, 3]);
+    expect(chest.getItems()).toEqual([1, 2, 3]);
+  });
+
+  it('should return the correct free capacity', () => {
+    chest.addItems([1, 2, 3]);
+    expect(chest.getFreeCapacity()).toBe(7);
+  });
+
+  it('should add items if there is enough free capacity', () => {
+    chest.addItems([1, 2, 3]);
+    expect(chest.getItems()).toEqual([1, 2, 3]);
+  });
+
+  it('should not add items if there is not enough free capacity (1)', () => {
+    chest.addItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    chest.addItems([11, 12]);
+    expect(chest.getItems()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it('should not add items if there is not enough free capacity (2)', () => {
+    chest.addItems([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    chest.addItems([11, 12, 13]);
+    expect(chest.getItems()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it('should remove an item', () => {
+    chest.addItems([1, 2, 3]);
+    chest.removeItem(2);
+    expect(chest.getItems()).toEqual([1, 3]);
+  });
+
+  it('should remove multiple items', () => {
+    chest.addItems([1, 2, 3, 4, 5]);
+    chest.removeItems([2, 4]);
+    expect(chest.getItems()).toEqual([1, 3, 5]);
   });
 });
--- projects/main-app/src/shared/classes/chest.ts
+++ projects/main-app/src/shared/classes/chest.ts
@@ -1,10 +1,34 @@
+import { getMatFormFieldMissingControlError } from "@angular/material/form-field";
+
 export class Chest {
 
     static instance: Chest = new Chest();
 
     maxCapacity: number = 10;
-    items: number[] = [1,33,789];
+    items: Set<number> = new Set<number>();
 
     private constructor(){}
+
+    getItems(): number[] {
+        return Array.from(this.items);
+    }
+
+    getFreeCapacity(): number {
+        return this.maxCapacity - this.items.size;
+    }
+
+    addItems(items: number[]): void {
+        if(this.getFreeCapacity() >= items.length) {
+            items.forEach(item => this.items.add(item));
+        } 
+    }
+
+    removeItem(item: number): void {
+        this.items.delete(item);
+    }
+
+    removeItems(items: number[]): void {
+        items.forEach(item => this.items.delete(item));
+    }
 
 }
