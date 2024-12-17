5ace6cc8cca7384a0d6aebde8b5f0e9970a405e8
viktorsirotin <vxod@sirotin.eu>
2024-12-17 17:02:24 +0100

    chore: #4. Report linked with variables.

--- projects/main-app/src/core/components/report/report.component.ts
+++ projects/main-app/src/core/components/report/report.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './report.component.css'
 })
 export class ReportComponent {
+  sertificate: string = "Certificate";
+  line1: string = "This certificate confirms";
+  line2: string = "that the bearer of this certificate is the legal virtual owner";
+  line3: string = "of the following remarkable numbers:";
+  numbers: string = "0, 12, 345, 7895, 888, 999";
+  signature: string = "Master and Lord of All Numbers Numericus Incalculable";
 
 }
