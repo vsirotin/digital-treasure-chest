8069bc03ea90807ff86d2c61327ed176fca8f9cd
viktorsirotin <vxod@sirotin.eu>
2025-01-06 17:51:22 +0100

    chore:  #9.  Language files created.

--- projects/main-app/src/core/components/info/info.component.ts
+++ projects/main-app/src/core/components/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as uiInfo from '../../../assets/languages/core/components/info/lang/1/en-EN.json';
 
 @Component({
   selector: 'app-info',
@@ -9,16 +10,7 @@ import { Component } from '@angular/core';
 })
 export class InfoComponent {
 
-  ui: IUIInfoComponent = {
-    greeting: 'Welcome to the digital treasure box!',
-    overview: 'This application will allow you to become an owner in priceless treasures and store them securely, but at the same time for free.',
-    explanation: 'These treasures are wonderful numbers. And you can store them in the memory of your browser.',
-    howToUse: 'You can select these treasures on the search page. Specify the interval and properties of the numbers you would like to possess. If too many numbers meet these criteria and all of them do not fit in your treasure chest, change the selection criteria. ',
-    whereExplanation: 'At the bottom of the selection page, you can see a brief description of the remarkable properties of the numbers you choose. ',
-    certificate: 'Then you can see a completed and signed certificate of ownership for those numbers. ',
-    settings: 'The settings page will help you to set the desired language if it did not happen automatically. ',
-    wisch: 'We wish you a pleasant time in selecting your numbers and further enjoyment in owning these treasures!'
-  };
+  ui: IUIInfoComponent = (uiInfo as any).default;
 
 }
 
