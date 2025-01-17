70a8bf069c2cfd4626b9de43e36aea35b6eda570
viktorsirotin <vxod@sirotin.eu>
2025-01-17 15:37:58 +0100

    chore:  #16. Using LocalStorageAdapter in Chest.

--- projects/main-app/src/shared/classes/chest.ts
+++ projects/main-app/src/shared/classes/chest.ts
@@ -11,12 +11,12 @@ export class Chest {
     chestChanged$ = new Subject<number[]>();
     private logger: ILogger = LoggerFactory.getLogger("shared/classes/Chest");
 
-    private adapter: LocalStorageAdapter<number[]> = new LocalStorageAdapter<number[]>("chest", []);
+    private adapter: LocalStorageAdapter<number[]> = new LocalStorageAdapter<number[]>("digital-chest", []);
     
     private items: Set<number> = new Set<number>([]);
 
     private constructor(){
-
+        this.items = new Set<number>(this.adapter.readValue());
     }
 
     getItems(): number[] {
@@ -31,24 +31,25 @@ export class Chest {
         if(this.getFreeCapacity() >= items.length) {
             items.forEach(item => this.items.add(item));
         } 
-        this.notifyNewState()
+        this.saveAndNotifyNewState()
     }
 
     removeItems(items: number[]): void {
         items.forEach(item => this.items.delete(item));
-        this.notifyNewState();
+        this.saveAndNotifyNewState();
     }
 
     replaceCurrentItemsWithNew(items: number[]): void {
         this.items.clear();
         this.addItems(items);
-        this.notifyNewState();
+        this.saveAndNotifyNewState();
 
     }
 
 
-    notifyNewState() {
+    saveAndNotifyNewState() {
         const newState = Array.from(this.items).sort((a, b) => a - b);
+        this.adapter.saveValue(newState);
         this.chestChanged$.next(newState);
         this.logger.log(`New chest state: ${Array.from(this.items)}`);
     }
